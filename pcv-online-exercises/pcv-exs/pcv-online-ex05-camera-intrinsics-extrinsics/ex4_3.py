70af9c49be847a570d4229cd4f0a218d056da7cc
Nacriema <huytruong2931998@gmail.com>
2021-12-07 23:58:05 +0700

    Update P3P method

--- pcv-online-exercises/pcv-exs/pcv-online-ex05-camera-intrinsics-extrinsics/ex4_3.py
+++ pcv-online-exercises/pcv-exs/pcv-online-ex05-camera-intrinsics-extrinsics/ex4_3.py
@@ -1,7 +1,9 @@
 # Compute camera extrinsics using P3P (spatial resection)
+from _multibytecodec import __create_codec
+
 import numpy as np
 import numpy.testing
-from numpy.linalg import norm, inv
+from numpy.linalg import norm, inv, svd, matrix_rank, det
 from itertools import combinations
 from math import sqrt
 
@@ -21,12 +23,78 @@ def _get_triplet(v, alpha, beta, a, b):
     s2_roots = np.roots(coeff)
 
     # TODO: Due to the result is > 0 then choose all, need to better config later
-    print(f's2 values: {s2_roots}')
+    # print(f's2 values: {s2_roots}')
     triplets = np.array([
         [s1, s2_roots[0], s3],
         [s1, s2_roots[1], s3]
     ])
     return triplets
+
+
+def _create_coeff(a, b, c, alpha, beta, gamma):
+    a2_add_c2_sur_b2 = (a**2 + c**2) / (b**2)
+    a2_sub_c2_sur_b2 = (a**2 - c**2) / (b**2)
+    b2_sub_c2_sur_b2 = (b**2 - c**2) / (b**2)
+    b2_sub_a2_sur_b2 = (b**2 - a**2) / (b**2)
+
+    A4 = (a2_sub_c2_sur_b2 - 1)**2 - (4 * (c**2 / b**2)) * np.cos(alpha)**2
+    A3 = 4 * (a2_sub_c2_sur_b2 * (1 - a2_sub_c2_sur_b2) * np.cos(beta) - (1 - a2_add_c2_sur_b2) * np.cos(alpha) * np.cos(gamma) + (2 * (c**2/b**2)) * np.cos(alpha)**2 * np.cos(beta))
+    A2 = 2 * (a2_sub_c2_sur_b2 ** 2 - 1 + 2 * (a2_sub_c2_sur_b2**2) * np.cos(beta)**2 + 2 * b2_sub_c2_sur_b2 * np.cos(alpha)**2 - (4 * a2_add_c2_sur_b2 * np.cos(alpha) * np.cos(beta) * np.cos(gamma)) + 2 * b2_sub_a2_sur_b2 * np.cos(gamma)**2)
+    A1 = 4 * (- a2_sub_c2_sur_b2 * (1 + a2_sub_c2_sur_b2) * np.cos(beta) + (2 * (a**2 / b**2)) * np.cos(gamma)**2 * np.cos(beta) - (1 - a2_add_c2_sur_b2) * np.cos(alpha) * np.cos(gamma))
+    A0 = (1 + a2_sub_c2_sur_b2) ** 2 - (4 * (a**2 / b**2)) * np.cos(gamma)**2
+
+    return A4, A3, A2, A1, A0
+
+
+# Add Umeyama method to solve Absolute Orientation problem
+def umeyama_absolute_orientation_solver(source_point_set, target_point_set):
+    """
+    This function will return the Rigid Transformation params to map from source_point_set to target_point_set by the given function:
+    See the original paper: http://web.stanford.edu/class/cs273/refs/umeyama.pdf
+    See Cyrill's lecture: https://www.youtube.com/watch?v=K5WG40gMlz8&list=PLgnQpQtFTOGTPQhKBOGgjTgX-mzpsOGOX&index=26
+
+    X_target = Lambda * R @ X_source + T (More info, see the code in create_sample_data.py)
+    Notice: The point set here have the same cardinality and has form of (x_n, y_n) pair x_n in source and y_n in target
+    :param source_point_set: np array shape (n_points, 3)
+    :param target_point_set: np array shape (n_points, 3)
+    :return: The params include:  scale, rotation matrix and translation vector
+    """
+    assert source_point_set.shape == target_point_set.shape, \
+        "Source point set and target point set must have the same shape !!!"
+
+    N, m = source_point_set.shape
+
+    # 1. Compute the mean point for each set - origin for each point set
+    # TODO: Here we not consider the weight for each point, in later version, we need take it into account
+    x_0 = np.mean(source_point_set, axis=0)
+    y_0 = np.mean(target_point_set, axis=0)
+
+    # 2. Compute scale factor, lambda
+    nominator = np.trace((target_point_set - y_0).T @ (target_point_set - y_0))
+    denominator = np.trace((source_point_set - x_0).T @ (source_point_set - x_0))
+    lbd = sqrt(nominator / denominator)
+
+    # 3. Compute the Rotation Matrix
+    a_n = source_point_set - x_0  # Shape (n_points, 3)
+    b_n = target_point_set - y_0  # Shape (n_points, 3)
+
+    cov_matrix = b_n.T.dot(a_n) / N  # Shape (3, 3)
+
+    u, d, v_t = svd(cov_matrix, full_matrices=True)
+    cov_rank = matrix_rank(cov_matrix)
+    s = np.eye(m)
+
+    if cov_rank >= m - 1 and det(cov_matrix) < 0:
+        s[m - 1, m - 1] = -1
+    elif cov_rank < m - 1:
+        raise ValueError("Collinearity detected in covariance matrix:\n{}".format(cov_matrix))
+
+    R = u.dot(s).dot(v_t)
+
+    # 4. Compute the Translation Matrix
+    T = y_0 - lbd * R @ x_0
+
+    return lbd, R, T
 
 
 def spatial_resection(x, X, K):
@@ -39,10 +107,11 @@ def spatial_resection(x, X, K):
     for point in x:
         point_hc = _normal_point_to_hc(point)
         vec = inv(K) @ point_hc
+        print(f'Vec = {vec}')
         unit_vec = vec / norm(vec)
         unit_vecs.append(-np.sign(c) * unit_vec)
     unit_vecs = np.stack(unit_vecs, axis=0)  # Shape (4, 3) w.r.t to the input point
-    print(f'Unit vectors: {unit_vecs}')
+    # print(f'Unit vectors: {unit_vecs}')
     # points_in_cam_coord = np.stack(points_in_cam_coord, axis=0)
     # print(f'Points in camera coordinate system: {points_in_cam_coord}')
 
@@ -55,45 +124,52 @@ def spatial_resection(x, X, K):
         distance_list.append(norm(X[pair[0]] - X[pair[1]]))
     angles_list = np.array(angles_list)
     distance_list = np.array(distance_list)
-    # Build Quadratic equation: A_4 * v^4 + A_3 * v^3 + A_2 * v^2 + A_1 * v_1 + A_0 = 0
-    a, b, c = distance_list
-    alpha, beta, gamma = angles_list
 
-    A4 = ((a**2 - c**2) / b**2 - 1)**2 - (4*(c**2) / b**2 * np.cos(alpha)**2)
-    A3 = 4 * ((a**2 - c**2)/b**2 * (1 - (a**2-c**2)/b**2) * np.cos(beta) - (1 - ((a**2 + c**2) / b**2))*np.cos(alpha)*np.cos(gamma) + 2 * c**2 / b**2 * np.cos(alpha)**2 * np.cos(beta))
-    A2 = 2 * ( ((a**2 - c**2)/b**2)**2 - 1 + 2*((a**2 - c**2)/b**2)**2 * np.cos(beta)**2 + 2 * ((b**2 - c**2)/b**2) * np.cos(alpha)**2 - 4 * ((a**2 + c**2)/b**2) * np.cos(alpha) * np.cos(beta) * np.cos(gamma) + 2 * ((b**2 - a**2)/b**2) * np.cos(gamma)**2)
-    A1 = 4 * ( -((a**2 - c**2)/b**2) * (1 + (a**2 - c**2) / b**2)*np.cos(beta) + 2*(a**2/b**2)*np.cos(gamma)**2 * np.cos(beta) - (1 - ((a**2 + c**2)/b**2)) * np.cos(alpha) * np.cos(gamma))
-    A0 = (1 + (a**2 - c**2)/b**2)**2 - 4 * a**2 / b**2 * np.cos(gamma)**2
+    print(f'Unit vectors: \n {unit_vecs}')
+    print(f'Angle list: \n {angles_list}')
+    print(f'Distance list: \n {distance_list}')
+    # Build Quadratic equation: A_4 * v^4 + A_3 * v^3 + A_2 * v^2 + A_1 * v_1 + A_0 = 0
+    c, b, a = distance_list
+    print(f'a = {a}, b = {b}, c = {c}')
+    gamma, beta, alpha = angles_list
+    print(f'Alpha = {alpha}, Beta = {beta}, Gamma = {gamma}')
+    A4, A3, A2, A1, A0 = _create_coeff(a, b, c, alpha, beta, gamma)
 
     coeff = np.array([A4, A3, A2, A1, A0])
     roots = np.roots(coeff)
-    print(roots)
+
+    # print(roots)
     # Get only the root with im part is zero
     result_set = [np.real(_) for _ in roots if np.imag(_) == 0]
-    print(result_set)
+    # print(result_set)
 
     # 2. Identify the correct solution using the 4th point
-    test_point = x[-1]
     triplets = []
     for v in result_set:
         triplets.append(_get_triplet(v, alpha, beta, a, b))
     triplets = np.vstack(triplets)
-    print(f'All triplets: {triplets}')
+    print(f'All triplets: \n{triplets}')
 
     # 3. Compute the coordinate transformation to obtain 𝑅 and 𝑋0
     # TODO: This is a problem and the link to it https://www.youtube.com/watch?v=roEjHhvLWmA \
     #  and the updated version: https://www.youtube.com/watch?v=K5WG40gMlz8
     # TODO: Suppose that I choose the triplet from step 2, this means I need a solution for step 2 !!!
     # Ok, I should make the code for "Absolute Orientation" problem. And then I'll go back to this
-    s1, s2, s3 = triplets[0]
-    R = np.eye(3, dtype=np.float)
-    X0 = np.zeros((3, 1), dtype=np.float)
-    return R, X0
+
+    # Uh mm, it seems and 1 and 2 was correct
+    s_ = triplets[1][:, np.newaxis]
+    X_i = unit_vecs[:3, ] * s_
+
+    print(f'X_i = \n {X_i}')
+    # Use Umeyama to compute the params for Absolute Transformation
+    lbd, R, X0 = umeyama_absolute_orientation_solver(source_point_set=X_i, target_point_set=X[:3, ])
+
+    return lbd, R, X0
 
 
 if __name__ == '__main__':
-    K = [[-1280, 0, 272],
-         [0, -1280, 480],
+    K = [[1280, 0, 272],
+         [0, 1280, 480],
          [0, 0, 1]]
 
     K = np.array(K, dtype=np.float)
@@ -108,4 +184,19 @@ if __name__ == '__main__':
                   [222.32934563, 634.74170641],
                   [61.33794639, 482.37484642]])
 
-    spatial_resection(x, X, K)
+    lbd, R, X0 = spatial_resection(x, X, K)
+    print(f'Lambda value: \n{lbd}')
+    print(f'Rotation matrix: \n{R}')
+    print(f'Translation matrix: \n{X0}')
+
+    # TEST
+    X0_ = -X0[:, np.newaxis]
+    I_X0 = np.concatenate((np.eye(3), X0_), axis=1)
+    P = lbd * K @ R.T @ I_X0
+
+    P_1 = np.array([7., 7., 7., 1.])
+    x_hat_1 = P @ P_1
+    x_hat_1 = x_hat_1 / x_hat_1[-1]
+
+    print("\n Reprojected image coordinates 1: \n", x_hat_1)
+
